e2b2ae3b69030b5579def8cbc7895c8d61c10e27
Marcel Haerry <haerry@puzzle.ch>
2008-05-28 18:49:19 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# dhcpd module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "dhcpd": }
+class dhcpd {}
