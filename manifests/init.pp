dbc44db3839953fa48fab16176d8ba5bdb87495a
Marcel Haerry <haerry@puzzle.ch>
2008-05-28 19:29:58 +0200

    init release

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,4 +6,44 @@
 
 
 # modules_dir { "dhcpd": }
-class dhcpd {}
+class dhcpd {
+    case $operatingsystem {
+        centos: { include dhcpd::centos }
+        default: { include dhcpd::base }
+    }
+}
+
+class dhcpd::base {
+    package{'dhcp':
+        ensure => present,
+    }
+
+    service{'dhcpd':
+        ensure => running,
+        enable => true,
+        hasstatus => true,
+        require => [ Package[dhcpd], File['/etc/dhcpd.conf'] ],
+    }
+
+    file{'/etc/dhcpd.conf':
+        source =>  [ "puppet://$server/files/dhcpd/${fqdn}/dhcpd",
+                    "puppet://$server/dhcpd/dhcpd.conf" ],
+        require => Package[dhcp],
+        notify => Service[dhcpd],
+        owner => root, group => 0, mode => '0644';
+    }
+}
+
+class dhcpd::centos {
+    file{'/etc/sysconfig/dhcpd':
+        source => [ "puppet://$server/files/dhcpd/sysconfig/${fqdn}/dhcpd",
+                    "puppet://$server/dhcpd/sysconfig/dhcpd" ],
+        require => Package[dhcp],
+        notify => Service[dhcpd],
+        owner => root, group => 0, mode => '0644';
+    }
+
+    Service[dhcpd]{
+        require +> File['/etc/sysconfig/dhcpd'],
+    } 
+}
